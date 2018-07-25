a41e43eabf26340e0338ca86f4f1b429cdf2e446
Larry Kirschner <larrykirschner@gmail.com>
2018-07-24 17:04:19 -0700

    added Constatne.STATUS_NOT_FOUND

--- Runtime/entities/Constants.cs
+++ Runtime/entities/Constants.cs
@@ -4,6 +4,7 @@ namespace BeatThat.Entities
     public static class Constants
     {
         public const string STATUS_OK = "ok";
+        public const string STATUS_NOT_FOUND = "not_found";
     }
 
 }
